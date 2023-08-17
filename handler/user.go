c084d62a2cbc533c5d30afeb398109e02efc1045
Rathief <rafie.fadlurahman@gmail.com>
2023-08-17 19:55:21 +0700

    changed some text in user.go

--- handler/user.go
+++ handler/user.go
@@ -17,25 +17,25 @@ type Handler struct {
 }
 
 // register
-func (h *Handler) RegisterUser(email,password string) string{
+func (h *Handler) RegisterUser(email, password string) string {
 	ctx := context.Background()
 	query := `SELECT email FROM customers WHERE email = ?;`
 
-	rows,err := h.UserHandler.QueryContext(ctx,query,email)
+	rows, err := h.UserHandler.QueryContext(ctx, query, email)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
-	if rows.Next(){
-		fmt.Println("Email is already exist")
+	if rows.Next() {
+		fmt.Println("Email already exists")
 		return email
 	}
 
-	hashedPassword,err := bcrypt.GenerateFromPassword([]byte(password),bcrypt.DefaultCost)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 
 	query2 := `INSERT INTO customers(email,password) VALUES (?,?)`
 
-	_,err = h.UserHandler.ExecContext(ctx,query2,email,string(hashedPassword))
+	_, err = h.UserHandler.ExecContext(ctx, query2, email, string(hashedPassword))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -43,22 +43,22 @@ func (h *Handler) RegisterUser(email,password string) string{
 }
 
 // login
-func (h *Handler) LoginUser(email,plainPassword string) error {
+func (h *Handler) LoginUser(email, plainPassword string) error {
 	ctx := context.Background()
 	query := `SELECT email,password FROM customers WHERE email = ?;`
-	rows,err := h.UserHandler.QueryContext(ctx,query,email)
+	rows, err := h.UserHandler.QueryContext(ctx, query, email)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
 
-	if rows.Next(){
+	if rows.Next() {
 		// get hashed password in database
 		var customer entity.Customers
-		rows.Scan(&customer.Email,&customer.Password)
+		rows.Scan(&customer.Email, &customer.Password)
 
 		// comparing hashed password in database with hashed password by user input
-		err := bcrypt.CompareHashAndPassword([]byte(customer.Password),[]byte(plainPassword))
+		err := bcrypt.CompareHashAndPassword([]byte(customer.Password), []byte(plainPassword))
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -74,19 +74,19 @@ func (h *Handler) SelectByEmail(email string) int {
 	ctx := context.Background()
 	query := `SELECT id FROM customers WHERE email = ?;`
 
-	rows,err := h.UserHandler.QueryContext(ctx,query,email)
+	rows, err := h.UserHandler.QueryContext(ctx, query, email)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
 	var customer_id int
-	if rows.Next(){
-		err:=rows.Scan(&customer_id)
+	if rows.Next() {
+		err := rows.Scan(&customer_id)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Println("selected id customer : ",customer_id)
+		fmt.Println("Selected customer ID: ", customer_id)
 	}
 	return customer_id
 }
@@ -96,44 +96,43 @@ func (h *Handler) InsertDefaultCustomersDetails(customer_id int) {
 	ctx := context.Background()
 	query := `INSERT INTO customers_details (customer_id) VALUES (?);`
 
-	_,err := h.UserHandler.ExecContext(ctx,query,customer_id)
+	_, err := h.UserHandler.ExecContext(ctx, query, customer_id)
 	if err != nil {
 		log.Fatal(err)
 	}
 	// debug
-	fmt.Println("success insert default customers details")
+	fmt.Println("Customer's detail inserted successfully")
 }
-
 
 // AFTER LOGIN
 
 // update customers details
-func (h *Handler) UpdateCustomersDetails(name string, age int,phone string,customer_id int) {
+func (h *Handler) UpdateCustomersDetails(name string, age int, phone string, customer_id int) {
 	ctx := context.Background()
 	query := `UPDATE customers_details SET name = ?, age = ?, phone = ? WHERE customer_id = ?;`
-	
-	_,err := h.UserHandler.ExecContext(ctx,query,name,age,phone,customer_id)
+
+	_, err := h.UserHandler.ExecContext(ctx, query, name, age, phone, customer_id)
 	if err != nil {
 		log.Fatal(err)
 	}
 	// debug
-	fmt.Println("Customers detail updated")
+	fmt.Println("Customer's detail updated successfully")
 }
 
 // view customers details
-func (h *Handler) ViewCustomersDetails(customer_id int) (*entity.Customers_details,error) {
+func (h *Handler) ViewCustomersDetails(customer_id int) (*entity.Customers_details, error) {
 	ctx := context.Background()
 	query := `SELECT name,age,phone FROM customers_details WHERE customer_id = ?`
 
-	rows,err := h.UserHandler.QueryContext(ctx,query,customer_id)
+	rows, err := h.UserHandler.QueryContext(ctx, query, customer_id)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
-	
+
 	var customer_details entity.Customers_details
-	if rows.Next(){
-		err := rows.Scan(&customer_details.Name,&customer_details.Age,&customer_details.Phone)
+	if rows.Next() {
+		err := rows.Scan(&customer_details.Name, &customer_details.Age, &customer_details.Phone)
 		if err != nil {
 			return nil, errors.New("Data is not available, please update your data")
 		}
@@ -141,7 +140,7 @@ func (h *Handler) ViewCustomersDetails(customer_id int) (*entity.Customers_detai
 		fmt.Println("Customers info success")
 
 	}
-	return &customer_details,nil
-	
+	return &customer_details, nil
+
 	// debug
 }
